4ed1a56b7e4de968c94d400c0b9f1e8924b9e2ac
Leandro Furtado <le_furtado@outlook.com.br>
2022-12-13 21:23:58 -0300

    initial commit

---
+++ src/components/PaginaPadrao/index.js
@@ -0,0 +1,14 @@
+import Navbar from 'components/Navbar';
+import { Outlet } from 'react-router-dom';
+import styles from './PaginaPadrao.module.scss';
+
+export default function PaginaPadrao() {
+    return (
+        <div className={styles.container}>
+            <Navbar />
+            <div className={styles['container-outlet']}>
+                <Outlet />
+            </div>
+        </div>
+    )
+}
