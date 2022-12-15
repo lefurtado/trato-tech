fd37b41af5fa96b1be38c035c81fc7e0dc042158
Leandro Furtado <le_furtado@outlook.com.br>
2022-12-14 21:32:16 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,26 @@
 import styles from './Footer.module.scss';
+import {
+    FaFacebook,
+    FaTwitter,
+    FaInstagram
+} from 'react-icons/fa';
+
+const iconeProps = {
+    color: 'white',
+    size: 24
+}
 
 export default function Footer() {
     return (
-        <footer>
-
+        <footer className={styles.footer}>
+            <div>
+                <FaFacebook {...iconeProps} />
+                <FaTwitter {...iconeProps} />
+                <FaInstagram {...iconeProps} />
+            </div>
+            <span>
+                Desenvolvido por Leandro Furtado.
+            </span>
         </footer>
     )
 }
